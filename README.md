1bbf6bb75f64f231061129fb52c6207d626caea3
atmtasum <atmatsum@yahoo-corp.jp>
2019-05-20 09:57:38 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+### abstruct
+5 steps for getting strava data
+
+### what you need
+php (>= 5.4), composer
+
+### 1
+```
+composer install
+```
+
+### 2
+```
+sudo CLIENT_SECRET=`cat secret/clientSecret.data` php -S 127.0.0.1:80
+```
+
+### 3
+then, access
+http://127.0.0.1/auth.php
+
+### 4
+get id and update getData.php
+
+### 5
+get data!!!
---
+++ auth.php
@@ -0,0 +1,34 @@
+<?php
+
+include 'vendor/autoload.php';
+
+use Strava\API\OAuth;
+use Strava\API\Exception;
+
+try {
+    $options = [
+        'clientId'     => 35202,
+        'clientSecret' => getenv('CLIENT_SECRET'),
+        'redirectUri'  => 'http://127.0.0.1/auth.php'
+    ];
+    $oauth = new OAuth($options);
+
+    if (!isset($_GET['code'])) {
+        print '<a href="'.$oauth->getAuthorizationUrl([
+            // Uncomment required scopes.
+            'scope' => [
+                'public',
+                // 'write',
+                'view_private',
+            ]
+        ]).'">Connect</a>';
+    } else {
+        $token = $oauth->getAccessToken('authorization_code', [
+            'code' => $_GET['code']
+        ]);
+        print $token->getToken();
+    }
+} catch(Exception $e) {
+     print $e->getMessage();
+}
+?>
